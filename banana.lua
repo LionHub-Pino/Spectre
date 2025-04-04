f88b09be8c2d5f7723b37c794b69ca9bc251aaf9
tulathangngu <fluxusvip@gmail.com>
2025-04-04 15:01:37 +0700

    Add files via upload

---
+++ banana.lua
@@ -0,0 +1,3 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().Key = "819aa4c678f66527e5c32905" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
---
+++ maru.lua
@@ -0,0 +1,3 @@
+getgenv().Key = "MARU-0MGRJ-5FPOU-V1FU-Q0BXK-0BWA7"
+getgenv().id = "915516837826015282"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
