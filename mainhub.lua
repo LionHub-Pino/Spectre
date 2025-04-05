aaca183e3dbc9cf7e78bd09c8e3b71d0602912a9
Pino <fluxusvip@gmail.com>
2025-04-05 20:13:59 +0700

    Create mainhub.lua

---
+++ mainhub.lua
@@ -0,0 +1,3 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().team = "Pirates" -- Marines
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e0c7fcf6c077fc23475cf4ce4db58e42.lua"))()
